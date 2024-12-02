fb1755572806a8c08dcae78ff0be2f0d74709da0
SpeedyM28 <jamil.bitarm76@gmail.com>
2024-12-02 03:50:00 -0500

    Completed exercise

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,7 +1,11 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const UseFetch = (url) => {
     const[data,setData]=useState();
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json()).then((data)=>setData(data))
+    },[])
+    return [data]
 }
 
 export default UseFetch
